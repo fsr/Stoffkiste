8afdbc71d516e88425927b4f7e1c402444c5f959
Sebastian Geisler <sgeisler@agdsn.de>
2017-08-16 22:43:03 +0200

    implement deploying

--- build.sh
+++ build.sh
@@ -2,17 +2,14 @@
 
 set -euo pipefail
 
-DEPLOY_REPOSITORY=""
-DEPLOY_BRANCH=""
-
 declare -a FILES
 
-if [[ $# != 1 ]]; then
+if [[ $# < 1 || $1 = "--help" ]]; then
     echo "Usage: $0 <command>"
     echo "Available commands are:"
-    echo "    build:  just builds all .tex files"
-    echo "    deploy: builds all .tex files an deploys to gh-pages"
-    echo "    clean:  deletes all build artefacts"
+    echo "    build:                  just builds all .tex files"
+    echo "    deploy <repo> <branch>: builds all .tex files an deploys to the given repository"
+    echo "    clean:                  deletes all build artefacts"
     exit 1
 fi
 
@@ -20,7 +17,7 @@ fi
 if [[ "$1" = "build" || "$1" = "deploy" ]]; then
     # building PDFs
     echo "building PDFs"
-    find .  -name "*.tex" -type f -print -execdir latexmk -quiet -halt-on-error -pdf {} \; || echo "failed"
+    find .  -name "*.tex" -type f -print -execdir latexmk -quiet -halt-on-error -pdf {} \;
 
 
     # building index file
@@ -52,13 +49,25 @@ if [[ "$1" = "build" || "$1" = "deploy" ]]; then
 EOF
 fi
 
-if [ "$1" = "deploy" ]; then
+if [[ "$1" = "deploy" ]]; then
+    if [[ $# != 3 ]]; then
+        echo "Error: you have to specify the target repository. See $0 --help ." >&2
+        exit 2
+    fi
+    DEPLOY_REPOSITORY=$2
+    DEPLOY_BRANCH=$3
     echo "deploying to $DEPLOY_REPOSITORY:$DEPLOY_BRANCH"
+    find .  -name "*.pdf" -type f -print -exec git add {} \;
+    CURR_COMMIT=$(git rev-parse HEAD)
+    git add index.htm
+    git commit -m "built PDFs and index"
+    git push -f $DEPLOY_REPOSITORY HEAD:$DEPLOY_BRANCH
+    git reset $CURR_COMMIT
 fi
 
-if [ "$1" = "clean" ]; then
+if [[ "$1" = "clean" || "$1" = "deploy" ]]; then
     echo "deleting PDF artefacts"
     find .  -name "*.tex" -type f -print -execdir latexmk -quiet -C -pdf {} \;
     echo "deleting index file"
-    rm index.htm 2> /dev/null
+    rm index.htm 2> /dev/null ||:
 fi
